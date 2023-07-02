3ffe8b818445339276e05fd8cbbb32d01b3822f3
jamieRollison <rollisonjamie15@gmail.com>
2023-07-02 12:20:31 -0700

    shamelessly steal an the interval idea from ellie's old portfolio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,20 @@
 <script>
-	const items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+	const testItems = ['a', 'b', 'c'];
+
+	const ms = 2000;
+	let idx = 0;
+	setInterval(() => {
+		if (idx < testItems.length - 1) {
+			idx++;
+		} else {
+			idx = 0;
+		}
+	}, ms);
 </script>
+
+<span class="flex justify-center space-x-1">
+	<h1 class="text-center text-light">I'm a</h1>
+	<h1 class="wheel text-center text-light w-40 border" style="animation-duration: {ms}ms;">
+		{testItems[idx]}
+	</h1>
+</span>

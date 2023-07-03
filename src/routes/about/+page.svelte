d4c2179ad691446a914c83aa7fb0e44cc0be22c8
jamieRollison <rollisonjamie15@gmail.com>
2023-07-03 16:17:10 -0700

    yup

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	const testItems = ['a', 'b', 'c'];
+	const items = ['linguist', 'web developer', 'leader', 'mobile dev', `"mathematician"`];
 
 	const ms = 2000;
 	let idx = 0;
 	setInterval(() => {
-		if (idx < testItems.length - 1) {
+		if (idx < items.length - 1) {
 			idx++;
 		} else {
 			idx = 0;
@@ -12,11 +12,15 @@
 	}, ms);
 </script>
 
-<span class="flex justify-center space-x-1">
-	<h1 class="text-center text-light">I'm a</h1>
-	<span class="border border-light overflow-hidden">
-		<h1 class="wheel text-center text-light w-40" style="animation-duration: {ms}ms;">
-			{testItems[idx]}
-		</h1>
+<div class="flex flex-col text-center text-light">
+	<span class="flex justify-evenly self-center w-80">
+		<h1>I'm a</h1>
+		<span class="border border-light overflow-hidden">
+			<h1 class="wheel text-center text-light w-40" style="animation-duration: {ms}ms;">
+				{items[idx]}
+			</h1>
+		</span>
 	</span>
-</span>
+	<h1 class="mt-20 text-3xl">I like making stuff</h1>
+	<p>come back for more</p>
+</div>

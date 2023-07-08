5293c7488180691dbe3728e2d5349a217a5eaf97
jamieRollison <rollisonjamie15@gmail.com>
2023-07-08 15:24:52 -0700

    starting hackathon info

---
+++ src/components/Hackathon/HackathonCard.svelte
@@ -0,0 +1,6 @@
+<script></script>
+
+<div class="h-80 w-80 bg-secondary border rounded flex-col flex text-center justify-center p-4">
+	<img src="ligo.jpg" alt="ligo title" class="w-fit h-fit border self-center" />
+	<h1 class="text-2xl font-extrabold">Ligo</h1>
+</div>

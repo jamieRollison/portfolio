fe890c47833f73d112600b9b17c223ecba94a51e
jamieRollison <rollisonjamie15@gmail.com>
2023-07-09 19:48:35 -0700

    more hackathon stuff

--- src/components/Hackathon/HackathonCard.svelte
+++ src/components/Hackathon/HackathonCard.svelte
@@ -3,4 +3,7 @@
 <div class="h-80 w-80 bg-secondary border rounded flex-col flex text-center justify-center p-4">
 	<img src="ligo.jpg" alt="ligo title" class="w-fit h-fit border self-center" />
 	<h1 class="text-2xl font-extrabold">Ligo</h1>
+	<h2 class="text-xl">Boilermake X</h2>
+	<h3 class="text-lg">sveltekit</h3>
+	<p>Ligo is a tool to track an event and its attendees' carbon footprint.</p>
 </div>

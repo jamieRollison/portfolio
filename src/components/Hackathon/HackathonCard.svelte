9352e809cbba6fdb1768404e467fbb296aa8794e
jamieRollison <rollisonjamie15@gmail.com>
2023-07-20 21:08:53 -0700

    yeah

--- src/components/Hackathon/HackathonCard.svelte
+++ src/components/Hackathon/HackathonCard.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="h-fit w-1/3 bg-light border rounded flex-col flex text-center justify-center p-4 m-2">
-	<h1 class="text-sm">⭐️ {win} ⭐️</h1>
+	<h1 class="text-sm mt-1">⭐️ {win} ⭐️</h1>
 	<div class="h-60 flex justify-center">
 		<img src={img} alt="{name} title" class="w-60 self-center min-40" />
 	</div>
